ed76e85b84d000edd0b67cefd87a332fb0b9fc65
SedemaOficina <oficinasecretaria.sedema@gmail.com>
2026-09-11 21:05:08 -0600

    G

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  *  - Nominatim, etc.: network-only
  * ============================================================ */
 
-const CACHE_VERSION = 'sia-v35-2026-09-12k';
+const CACHE_VERSION = 'sia-v35-2026-09-12m';
 const CACHE_RUNTIME = 'sia-runtime-v35';
 const CACHE_DATA    = 'sia-data-v35';
 
